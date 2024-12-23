bd74e7d0eb980444803833a848a9fef4667d2c8e
arias-ds3 <ariasfa7@gmail.com>
2024-12-23 20:03:26 -0300

    initial structure and router permissionsRouter

---
+++ data/permissions.js
@@ -0,0 +1,6 @@
+let permissions = [
+    { id:1, text: "hacer una compra", approdedBy:[], userId: 1 },
+    { id:2, text: "hacer una compra", approdedBy:[], userId: 2 }
+]
+
+module.exports = permissions
