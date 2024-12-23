9d12745a47ba9d2b782ad1489edb8ae953de9d18
arias-ds3 <ariasfa7@gmail.com>
2024-12-23 20:31:19 -0300

    post /permissions

--- routers/routerPermissions.js
+++ routers/routerPermissions.js
@@ -7,5 +7,24 @@ routerPermissions.get("/", (req, res) => {
     res.json(permissions)
 })
 
+routerPermissions.post("/", (req, res) => {
+    let text = req.body.text
+    let userId = req.body.userId
+
+    let lastId = permissions[permissions.length-1].id
+
+    permissions.push({ 
+        id: lastId+1,
+         text: "hacer una compra", 
+         approdedBy:[],
+          userId: 1
+        })
+
+        res.json({ id: lastId+1 })
+        // 1 { clave: valor }
+
+})
+
+
 
 module.exports = routerPermissions
