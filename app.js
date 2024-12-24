936e293ba236ad1a1b324edbbd26cd78059055fa
arias-ds3 <ariasfa7@gmail.com>
2024-12-23 22:24:04 -0300

    router users and POST /permissions with password and email

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ app.use(express.json())
 
 
 let routerPermissions = require("./routers/routerPermissions")
+let routerUsers = require("./routers/routerUsers")
 app.use("/permissions", routerPermissions)
-
+app.use("/users", routerUsers)
 
 
 app.listen(port, () => {
